32d09e4dbd4c7ee03e644e278f52952719eba6cf
JUAN FERNANDO PEREZ RESTREPO <juanperez286997@correo.itm.edu.co>
2024-05-23 19:45:12 -0500

    pequeño cambio

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config({path: '.env'})
 
 const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS, {
-    host: process.env.BD_HOST,
+    host: process.env.BD_HOST || '0.0.0.0',
     port: 3306,
     dialect: 'mysql',
     define: {
