974b27bd4c58d6b90c48a808b81fe3dc19f0008b
jfperez95 <101363904+jfperez95@users.noreply.github.com>
2024-05-23 21:50:04 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 import Sequelize from "sequelize";//Concetar y configurar la db
 import dotenv from 'dotenv'
-dotenv.config({path: '.env'})
+//dotenv.config({path: '.env'})
 
 const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS, {
-    host: process.env.BD_HOST || '0.0.0.0',
+    host: process.env.BD_HOST,
     port: 3306,
     dialect: 'mysql',
     define: {
